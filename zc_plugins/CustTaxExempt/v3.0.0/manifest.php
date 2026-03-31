d6340dba0404aa9ca06e86dd2952f503374e4252
lat9 <cindy@vinosdefrutastropicales.com>
2026-03-31 11:43:49 -0400

    Move existing files into encapsulated structure
    - Remove uninstall SQL
    - Move admin's `init_includes` file to `ScriptedInstaller`

---
+++ zc_plugins/CustTaxExempt/v3.0.0/manifest.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'pluginVersion' => 'v3.0.0',
+    'pluginName' => 'Customer Tax Exempt',
+    'pluginDescription' => 'This plugin enables an admin to indicate that a customer is exempt from taxes.',
+    'pluginAuthor' => 'Vinos de Frutas Tropicales (lat9)',
+    'pluginId' => 2259, // ID from Zen Cart forum
+    'zcVersions' => [],
+    'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
+    'github_repo' => '', // url
+    'pluginGroups' => [],
+    'removesUnencapsulatedVersion' => true, // !empty presence indicates that non-encapsulated files are removed during install
+];
